058c371f5dc3ed8e8575f873cfd6b0b718ed6718
John Melody Me <johnmelodyme@icloud.com>
2019-12-30 23:39:39 +0800

    Upload

---
+++ src/com/johnmelodyme/account/Main.java
@@ -0,0 +1,24 @@
+package com.johnmelodyme.account;
+
+import javax.swing.*;
+
+/**
+ * @AUTHOR: JOHN MELODY ME
+ * @COPYRIGHT: ALL RIGHT RESERVED © JOHN MELODY MELISSA COPYRIGHT
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame;
+        frame = new JFrame("Meac -- Accounting Software.");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,600);
+        frame.setVisible(true);
+
+        /*
+        String pathtofile;
+        pathtofile = "../../";
+        ImageIcon img = new ImageIcon(pathtofile);
+        frame.setIconImage(img.getImage());
+         */
+    }
+}
